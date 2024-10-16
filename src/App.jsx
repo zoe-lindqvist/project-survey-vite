3bc113ee676e093e09b88be43e6f82fd0fa3afee
jacquelinekellyhunt <kelly@cosafe.com>
2024-10-16 17:52:00 +0300

    Clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import WelcomePage from './components/WelcomeFolder/WelcomePage.jsx';
 import QuestionPage from './components/QuestionFolder/Question.jsx';
 import './App.css';
@@ -5,14 +6,12 @@ import './App.css';
 const App = () => {
   const [startQuiz, setStartQuiz] = useState(false);
 
-  // Function to handle the quiz start
   const handleStartQuiz = () => {
-    setStartQuiz(true);  // This will change the state to start the quiz
+    setStartQuiz(true);  
   };
 
   return (
     <div className="App">
-      {/* Pass the handleStartQuiz function as a prop to WelcomePage */}
       {!startQuiz ? (
         <WelcomePage onStartQuiz={handleStartQuiz} />) : (
         <QuestionPage />
