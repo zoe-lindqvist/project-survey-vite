d65ab627b1f451214e7ddb43de78166ce5de01c3
Zoe <zoe.lindqvist@gmail.com>
2024-10-16 16:58:07 +0200

    confirmation HTML

--- src/components/ConfirmationFolder/ConfirmationPage.jsx
+++ src/components/ConfirmationFolder/ConfirmationPage.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import './ConfirmationPage.css';
+import "./ConfirmationPage.css";
 
-const ConfirmationScreen = () => {
+export const ConfirmationScreen = () => {
   return (
-    <div className="confirmation-screen">
-      <h1 className="app-title">Paw Pops</h1>
-      <div className="confirmation-star">
+    <section className="confirmation-screen">
+      <header>
+        <h1 className="app-title">Paw Pops</h1>
+      </header>
+      <figure className="confirmation-star">
         <img src="../assets/star.png" alt="Happy Star" className="star-image" />
-      </div>
+      </figure>
       <h2 className="score-title">Purr-fect Score!</h2>
       <p className="confirmation-message">
         Your pawsome performance has set a new standard of excellence!
       </p>
       <div className="score-badge">
         <span className="lightning-icon">
-          <img src="../assets/lightning.png" alt="Score" className="score-image" />
+          <img
+            src="../assets/lightning.png"
+            alt="Score"
+            className="score-image"
+          />
         </span>
         <span className="score-points"></span>
       </div>
-    </div>
+    </section>
   );
 };
-
-export default ConfirmationScreen;
