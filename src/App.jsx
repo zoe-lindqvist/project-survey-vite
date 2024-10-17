57e0ad35448b2e7f425aa8dfbd08eec6d6961f8a
Zoe <zoe.lindqvist@gmail.com>
2024-10-17 13:48:38 +0200

    question3

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
-import WelcomePage from './components/WelcomeFolder/WelcomePage.jsx';
-import QuestionOne from './components/QuestionOne/QuestionOne';
-import QuestionTwo from './components/QuestionTwo/QuestionTwo.jsx';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import WelcomePage from "./components/WelcomeFolder/WelcomePage.jsx";
+import QuestionOne from "./components/QuestionOne/QuestionOne";
+import QuestionTwo from "./components/QuestionTwo/QuestionTwo.jsx";
+import QuestionThree from "./components/QuestionThree/QuestionThree.jsx";
+
+import "./App.css";
 
 const App = () => {
   const [startQuiz, setStartQuiz] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0); // This tracks the current question
 
   const handleStartQuiz = () => {
-    setStartQuiz(true);  
+    setStartQuiz(true);
   };
 
   const handleNextQuestion = () => {
     setCurrentQuestion(currentQuestion + 1);
-  }
- 
+  };
+
   return (
     <div className="App">
       {!startQuiz ? (
@@ -24,12 +27,22 @@ const App = () => {
         <>
           {currentQuestion === 0 && <QuestionOne onNext={handleNextQuestion} />}
           {currentQuestion === 1 && <QuestionTwo onNext={handleNextQuestion} />}
-          {currentQuestion === 2 && <QuestionThree onNext={handleNextQuestion} />}
-          {currentQuestion === 3 && <QuestionFour onNext={handleNextQuestion} />}
-          {currentQuestion === 4 && <QuestionFive onNext={handleNextQuestion} />}
+          {currentQuestion === 2 && (
+            <QuestionThree onNext={handleNextQuestion} />
+          )}
+          {currentQuestion === 3 && (
+            <QuestionFour onNext={handleNextQuestion} />
+          )}
+          {currentQuestion === 4 && (
+            <QuestionFive onNext={handleNextQuestion} />
+          )}
           {currentQuestion === 5 && <QuestionSix onNext={handleNextQuestion} />}
-          {currentQuestion === 6 && <QuestionSeven onNext={handleNextQuestion} />}
-          {currentQuestion === 7 && <QuestionEight onNext={handleNextQuestion} />}
+          {currentQuestion === 6 && (
+            <QuestionSeven onNext={handleNextQuestion} />
+          )}
+          {currentQuestion === 7 && (
+            <QuestionEight onNext={handleNextQuestion} />
+          )}
         </>
       )}
     </div>
